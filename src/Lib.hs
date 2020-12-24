e6304a6e4e3d8609c50949b74f044c8b02300b5a
Haruto Watanabe <w.haru10n@gmail.com>
2020-12-24 23:23:23 +0900

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,7 @@ module Lib
 
 import Servant(serve, Proxy(..), Server, JSON, Get, (:>))
 import Data.Aeson(ToJSON)
-import Data.Time.Calendar
+import Data.Time (UTCTime)
 import GHC.Generics(Generic)
 import Network.Wai(Application)
 import Network.Wai.Handler.Warp(run)
